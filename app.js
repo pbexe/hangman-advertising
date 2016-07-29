160d6f583ddad376fee28154fe06ad293f94a6c0
Finnian Anderson <finnian@fxapi.co.uk>
2016-07-29 15:22:16 +0100

    Integrations with front end

--- app.js
+++ app.js
@@ -9,35 +9,36 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	console.log("Sending root");
+	res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
+	fs.readFile("/Users/finnian/Pictures/profile.jpg", function(err, squid){
+		if (err) throw err;
+		img = new Image;
+		img.src = squid;
+
+		for (var i = 0; i < 1000; i++){
+			ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
+			ctx.font = '30px Impact';
+			ctx.rotate(.1);
+			ctx.fillText("Awesome! " + i, 50, 100);
+
+			var te = ctx.measureText('Awesome! ' + i);
+			ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+			ctx.beginPath();
+			ctx.lineTo(50, 102);
+			ctx.lineTo(50 + te.width, 102);
+			ctx.stroke();
+
+			io.sockets.emit("image", canvas.toDataURL());
+			console.log("Rendered image");
+		}
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-fs.readFile("/Users/finnian/Pictures/profile.jpg", function(err, squid){
-	if (err) throw err;
-	img = new Image;
-	img.src = squid;
-
-	for (var i = 0; i < 1000; i++){
-		ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
-		ctx.font = '30px Impact';
-		ctx.rotate(.1);
-		ctx.fillText("Awesome! " + i, 50, 100);
-
-		var te = ctx.measureText('Awesome! ' + i);
-		ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-		ctx.beginPath();
-		ctx.lineTo(50, 102);
-		ctx.lineTo(50 + te.width, 102);
-		ctx.stroke();
-
-		io.sockets.emit("image", canvas.toDataURL())
-	}
+	console.log('listening on *:3000');
 });
