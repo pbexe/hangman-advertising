65514c6365c49c4c6532f64a63a102c3cd4f8da9
Finnian Anderson <finnian@fxapi.co.uk>
2016-07-29 23:48:35 +0100

    Add base code for reading QR codes

--- modules/clients.js
+++ modules/clients.js
@@ -1,14 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var QRCode = require('qrcode');
+var QRCodeGenerator = require('qrcode');
+var QRCodeReaderModule = require('qrcode-reader');
+var QRCodeReader = new QRCodeReaderModule();
 
 var socketClients = [];
+
+var uploading = multer({
+	dest: __dirname + '../public/uploads/',
+})
 
 // Configuring Routes
 app.get('/', function(req, res){
 	console.log("Sending root");
 	res.sendfile('slave.html');
+});
+
+app.post("/upload", function(req, res){
+	console.log(req.files);
 });
 
 // Socket.IO
@@ -20,7 +30,7 @@ io.on('connection', function(socket){
 	
 	socket.on("get code", function(packet, callback){
 		console.log("Generating code for " + socket.id + " (" + socketClients.indexOf(socket.id) + ")");
-		QRCode.toDataURL(socketClients.indexOf(socket.id).toString(), function(err, url){
+		QRCodeGenerator.toDataURL(socketClients.indexOf(socket.id).toString(), function(err, url){
 			callback(url);
 		});
 	});
@@ -33,6 +43,10 @@ io.on('connection', function(socket){
 			}
 		}));
 	});*/
+
+	socket.on("disconnect", function(packet){
+		socketClients.splice(socketClients.indexOf(socket.id), 1);
+	});
 });
 
 // Create HTTP Server
