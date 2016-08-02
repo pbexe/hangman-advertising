6d18926e53fc7ce991570bd8e970f562a894c3ff
Finnian Anderson <finnian@fxapi.co.uk>
2016-08-02 22:02:58 +0100

    Formatting fixes
    Well you know, I might as well format the lot nicely instead of just
    one.
    Sorry @popey456963, we’re going with tabs here :heart:

--- modules/clients.js
+++ modules/clients.js
@@ -189,91 +189,92 @@ function sendDisplayPoints(id, v1, v2, v3, v4) {
 }
 
 function getSocketIndex(id) {
-    for (var i = 0; i < socketClients.length; i++) {
-        if (socketClients[i].id == id) return i;
-    }
-    return false;
+	for (var i = 0; i < socketClients.length; i++) {
+		if (socketClients[i].id == id) return i;
+	}
+	return false;
 }
 
 function configure() {
-    for (var i = 0; i < socketClients.length; i++) {
-        socketClients[i].checked = false;
-    }
-    io.emit("configure");
+	rendering = false;
+	for (var i = 0; i < socketClients.length; i++) {
+		socketClients[i].configured = false;
+	}
+	io.emit("configure");
 }
 
 function checkConfigured() {
-    for (var i = 0; i < socketClients.length; i++) {
-        if (socketClients[i].checked === false) {
-            return false;
-        }
-    }
-    return true;
+	for (var i = 0; i < socketClients.length; i++) {
+		if (socketClients[i].configured === false) {
+			return false;
+		}
+	}
+	return true;
 }
 
 function render() {
-    io.emit("render");
+	io.emit("render");
 }
 
 // Socket.IO
 io.on('connection', function(socket) {
-    console.log("New connection, reconfiguring clients...");
-    if (getSocketIndex(socket.id) === false) {
-        socketClients.push({
-            id: socket.id,
-            checked: false,
-            width: 0,
-            height: 0
-        });
-    }
+	console.log("New connection, reconfiguring clients...");
+	if (getSocketIndex(socket.id) === false) {
+		socketClients.push({
+			id: socket.id,
+			configured: false,
+			width: 0,
+			height: 0
+		});
+	}
 
-    configure();
+	configure();
 
-    socket.on("constraints", function(packet) {
-        var index = getSocketIndex(socket.id);
-        if (index !== false) {
-            socketClients[index].checked = true;
-            socketClients[index].width = packet.width;
-            socketClients[index].height = packet.height;
-        }
-        if (checkConfigured()) {
-            console.log("Configuration done, switching to render mode");
-            render();
-        }
-    });
+	socket.on("constraints", function(packet) {
+		var index = getSocketIndex(socket.id);
+		if (index !== false) {
+			socketClients[index].configured = true;
+			socketClients[index].width = packet.width;
+			socketClients[index].height = packet.height;
+		}
+		if (checkConfigured()) {
+			console.log("Configuration done, switching to render mode");
+			// render();
+		}
+	});
 
-    socket.on("get code", function(packet, callback) {
-        QRCodeGenerator.draw(getSocketIndex(socket.id).toString(), function(err, canvas) {
-            Jimp.read(canvas.toBuffer(), function(err, image) {
-                for (var y = 0; y < image.bitmap.width; y++) {
-                    for (var x = 0; x < image.bitmap.height; x++) {
-                        if (image.getPixelColor(x, y) == parseInt("FFFFFFFF", 16)) {
-                            image.setPixelColor(parseInt("DF298AFF", 16), x, y);
-                        } else if (image.getPixelColor(x, y) == parseInt("000000FF", 16)) {
-                            image.setPixelColor(parseInt("FFFFFFFF", 16), x, y);
-                        }
-                    }
-                }
-                image.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
-                    callback("data:image/png;base64," + buffer.toString("base64"));
-                });
-            });
-        });
-    });
+	socket.on("get code", function(packet, callback) {
+		QRCodeGenerator.draw(getSocketIndex(socket.id).toString(), function(err, canvas) {
+			Jimp.read(canvas.toBuffer(), function(err, image) {
+				for (var y = 0; y < image.bitmap.width; y++) {
+					for (var x = 0; x < image.bitmap.height; x++) {
+						if (image.getPixelColor(x, y) == parseInt("FFFFFFFF", 16)) {
+							image.setPixelColor(parseInt("DF298AFF", 16), x, y);
+						} else if (image.getPixelColor(x, y) == parseInt("000000FF", 16)) {
+							image.setPixelColor(parseInt("FFFFFFFF", 16), x, y);
+						}
+					}
+				}
+				image.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+					callback("data:image/png;base64," + buffer.toString("base64"));
+				});
+			});
+		});
+	});
 
-    socket.on("disconnect", function(packet) {
-        socketClients.splice(getSocketIndex(socket.id), 1);
-        configure();
-    });
+	socket.on("disconnect", function(packet) {
+		socketClients.splice(getSocketIndex(socket.id), 1);
+		configure();
+	});
 });
 
 // Create HTTP Server
 http.listen(3000, function() {
-    console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
 
 process.on("message", function(message) {
-    if (rendering == true) {
-        io.emit("frame", JSON.parse(message));
-    }
+	if (rendering == true) {
+		io.emit("frame", JSON.parse(message));
+	}
 });
